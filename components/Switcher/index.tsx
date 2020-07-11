2b7130e1100dccb28730264e70d9e3a4553713c2
elis_alech <elis.alech900@gmail.com>
2020-07-11 18:53:42 +0300

    tsc fixed

--- components/Switcher/index.tsx
+++ components/Switcher/index.tsx
@@ -3,6 +3,9 @@ import ExtendIcon from "../icons/ExtendIcon";
 
 interface Props {
   isOpen: boolean;
+  onClick?: () => void;
+  className?: string;
+  fill?: string;
 }
 
 export default ({ isOpen, ...props }: Props) =>
