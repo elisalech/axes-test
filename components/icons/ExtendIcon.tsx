38321fc1146afa12479e05c893b1e748a277f029
elis_alech <elis.alech900@gmail.com>
2020-07-11 18:56:33 +0300

    tsc fixed

--- components/icons/ExtendIcon.tsx
+++ components/icons/ExtendIcon.tsx
@@ -1,4 +1,10 @@
-export default (props) => (
+interface Props {
+  onClick?: () => void;
+  className?: string;
+  fill?: string;
+}
+
+export default (props: Props) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     viewBox="0 0 24 24"
